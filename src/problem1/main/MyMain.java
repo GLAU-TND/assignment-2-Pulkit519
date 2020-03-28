7eb6f47552198f9ad2ebb9b5d8ac365a43eee9da
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-03-28 17:14:37 +0530

    complete the implementation of ourTraversal method.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,18 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
 // executable class
+import problem1.mybst.MyBinarySearchTree;
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(25);
+        tree.insert(15);
+        tree.insert(35);
+        tree.insert(11);
+        tree.insert(20);
+        tree.insert(34);
+        tree.insert(40);
+
+        tree.ourTraversal();
+    }
 }
