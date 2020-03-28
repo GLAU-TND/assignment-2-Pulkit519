280a296f418f9e879397de22b8f419e9ad3dbe53
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-03-28 17:27:27 +0530

    creating toString method in Student class

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,10 +1,44 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.student;
 // to store student information and properties
 public class Student {
+
+    private String name;
+    private int backLog;
+    private int appearingCount;
+
+    public Student(String name,int backLog,int appearingCount){
+        this.appearingCount=appearingCount;
+        this.backLog=backLog;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBackLog() {
+        return backLog;
+    }
+
+    public void setBackLog(int backLog) {
+        this.backLog = backLog;
+    }
+
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public void setAppearingCount(int appearingCount) {
+        this.appearingCount = appearingCount;
+    }
+
+    @Override
+    public String toString(){
+        return getName()+" "+getBackLog()+" "+getAppearingCount();
+    }
+
 }
