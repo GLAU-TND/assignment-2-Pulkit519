1464c85feb47b0b9e0d0db23fffc8e5f659562e7
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-03-28 17:16:00 +0530

    implement both preOrder and postOrder Traversal to check given statement

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -1,11 +1,24 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:15 PM
- */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(25);
+        tree.insert(15);
+        tree.insert(35);
+        tree.insert(11);
+        tree.insert(20);
+        tree.insert(34);
+        tree.insert(40);
+
+        System.out.println("pre Order Traversal");
+        tree.preOrderTraversal();
+
+        System.out.println("\n\npost Order Traversal");
+        tree.postOrderTraversal();
+
+    }
 }
