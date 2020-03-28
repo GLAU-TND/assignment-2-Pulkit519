10f8e92ca073b88da5306e5a467abb73e174843e
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2020-03-28 17:19:43 +0530

    override the toString method in Node class for Queue(problem3).

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,42 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.node;
 
-public class Node {
+public class Node<T> {
+
+    private T data;
+    private Node next;
+    private int priority;
+
+    public Node(T data, int priority) {
+        this.data = data;
+        this.priority = priority;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString(){
+        return data.toString();
+    }
 }
